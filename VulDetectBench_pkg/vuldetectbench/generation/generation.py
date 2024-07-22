5e134f23a1d1aad358027b41c0a5b402aea3c856
HeartyHaven <gaolang1643@hust.edu.cn>
2024-07-22 03:35:04 +0000

    single task eval

---
+++ VulDetectBench_pkg/vuldetectbench/generation/generation.py
@@ -0,0 +1,31 @@
+# 代码结构
+# 数据集转化为prompt的工具
+# prompt输送给模型，模型输出
+# 输出的测试
+# 要实现的基本工具
+class LLM_for_Generation:
+    def __init__():
+        pass
+    def api_generate():
+        pass
+    def hf_generate():
+        pass
+    def generate():
+        pass
+    def get_task_info():
+        
+        pass
+    def _run_single_task():
+        pass
+    def run_exist_detect():
+        pass
+    def run_cls():
+        pass
+    def run_root_cause_detect():
+        pass
+    def run_trigger_point_detect():
+        pass
+    def run_obj_func_detect():
+        pass
+    def save_results():
+        pass
